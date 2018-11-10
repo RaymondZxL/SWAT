4e5f759ce0d41593404387f701ec58fd12027605
Raymond Zhou <x1zhou@eng.ucsd.edu>
2018-11-10 15:46:03 -0800

    third commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet, Image, KeyboardAvoidingView } from 'react-native';
 
 export default class App extends Component {
 
@@ -17,45 +17,64 @@ export default class App extends Component {
 
   render() {
     return (
+      <KeyboardAvoidingView style={styles.container} behavior="position">
       <View style={styles.container}>
-      <Text style={styles.titleText}>Hi, Welcome To</Text>
-        <Text style={styles.titleText}>Momento</Text>
+      <Image style={styles.logo} source={require("./assets/IMG_2197.jpg")}/>
+      <Text style={styles.textBox}>Email:</Text>
         <TextInput
           value={this.state.email}
           keyboardType = 'email-address'
           onChangeText={(email) => this.setState({ email })}
-          placeholder='email'
-          placeholderTextColor = 'white'
+          placeholder='example@ucsd.edu'
+          placeholderTextColor = 'black'
           style={styles.input}
         />
+        <Text style={styles.textBox}>Password:</Text>
         <TextInput
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
-          placeholder={'password'}
+          placeholder={'example'}
           secureTextEntry={true}
-          placeholderTextColor = 'white'
+          placeholderTextColor = 'black'
           style={styles.input}
         />
         
      
         <TouchableOpacity
-          style={styles.button}
+          style={styles.button1}
           onPress={this.onLogin.bind(this)}
        >
          <Text style={styles.buttonText}> Sign Up / Login </Text>
        </TouchableOpacity>
+
+       <TouchableOpacity
+          style={styles.button2}
+          onPress={this.onLogin.bind(this)}
+       >
+         <Text style={styles.buttonText}> Forgot Password </Text>
+       </TouchableOpacity>
         
       </View>
+      </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  logo: {
+  	height: 300
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'salmon',
+    backgroundColor: 'white',
+  },
+  textBox: {
+    fontFamily: 'Baskerville',
+    fontSize: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   titleText:{
     fontFamily: 'Baskerville',
@@ -63,10 +82,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
+  button1: {
     alignItems: 'center',
     backgroundColor: 'powderblue',
     width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 25,
+    marginBottom: 10,
+  },
+  button2: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: 150,
     height: 44,
     padding: 10,
     borderWidth: 1,
