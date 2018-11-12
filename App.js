99e77d207e8c87429536148e98de97d302f77d5d
martinmags <mmagsomb@ucsd.edu>
2018-11-11 16:13:04 -0800

    fix export, added files, and started home.js

--- App.js
+++ App.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet, Image, KeyboardAvoidingView } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
-import firebase from 'firebase'
+import firebase from './src/firebase'
 
 import Login from './src/Login'
 import Main from './src/Main'
 import NewUser from './src/NewUser'
 import Password from './src/Password'
+import Home from './src/Home'
 
 const App = createStackNavigator({
 	Main: {screen: Main},
 	Login:{screen:Login},
 	NewUser: {screen: NewUser},
 	Password: {screen: Password},
+  Home: {screen: Home},
 });
 
 {/*Change to separate firebase.js file if issues occur*/}
-const firebaseConfig = {
+{/*const firebaseConfig = {
   apiKey: "AIzaSyBN0Va-Agm8vCEH7mBQsb6nhByWfKFx4EA",
   authDomain: "ios-swat.firebaseapp.com",
   databaseURL: "https://ios-swat.firebaseio.com",
@@ -26,6 +28,6 @@ const firebaseConfig = {
   messagingSenderID: "1015403830821"
 };
 firebase.initializeApp(config);
+*/}
 
-export default firebase;
 export default App;
